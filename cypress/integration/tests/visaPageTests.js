707ed615906c7030dc2ac88c66235f0df03ec68d
Mihailo <mikhail.lo990@gmail.com>
2022-05-08 17:43:29 +0200

    Innitial commit

--- cypress/integration/tests/visaPageTests.js
+++ cypress/integration/tests/visaPageTests.js
@@ -8,18 +8,14 @@ describe('Visa page tests', function() {
     beforeEach('Navigate to Login page and log in ', function() {
         cy.visit(Cypress.env("baseUrl"))
         cy.loginToSite(customerUser.username, customerUser.password) 
-        // cy.get(topHeader.login).click()
-        // cy.get(form.email).type(customerUser.username, {force:true})
-        // cy.get(form.password).type(customerUser.password,{force:true})
-        // cy.get(form.login).click({force: true})
     });
 
-    it.only('Check if the submit visa app works', function(){
+    it('Check if the submit visa app works', function(){
         cy.get(mainMenu.visa).click({force:true})
         cy.url().should('include', '/visa')
     }) 
 
-    it.only('Test to see if the Visa page leads the user to the Visa submission', function(){
+    it('Test to see if the Visa page leads the user to the Visa submission', function(){
         
         cy.get(mainMenu.visa).click({force:true})
         cy.get('#from_country').select("AF",{force:true})
